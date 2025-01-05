bd9251dcfb478b3e8ba43f05e129a8961dbd40a5
HyperionU <108021531+HyperionU@users.noreply.github.com>
2025-01-05 22:20:50 +0000

    (www): add navigation menu content

---
+++ apps/www/src/lib/content.ts
@@ -0,0 +1,46 @@
+import type { Project } from "./utils";
+
+export const projectList: Project[] = [
+  {
+    title: "Nota-Set",
+    href: "https://dub.sh/Nota-Set",
+    description:
+      "A repository of notes and presentations by Hyperion University.",
+  },
+  {
+    title: "HyperionU Live",
+    href: "https://git.new/huLive",
+    description:
+      "A series of live lectures diving into the depths of mathematics.",
+  },
+  {
+    title: "BCGrad",
+    href: "https://git.new/bcGrad",
+    description: "Information about the complex process of Graduating in BC.",
+  },
+  {
+    title: "HUVirtual",
+    href: "#",
+    description: "(Free) Virtual courses by Hyperion University.",
+  },
+  {
+    title: "DCMChallenges",
+    href: "#",
+    description: "A set of Math & Computing competitions from Decrypt Maths",
+  },
+  {
+    title: "SIGFigures",
+    href: "#",
+    description: "An event made for sharing new MSEC Information.",
+  },
+  {
+    title: "Calculus Games",
+    href: "/calc",
+    description: "Our custom versions of games, based on Calculus",
+  },
+  {
+    title: "An Intro To Lean",
+    href: "https://adam.math.hhu.de/#/g/hyperionu/hulean",
+    description: "Our custom game to introduce Lean4 math and logic.",
+  },
+];
--- apps/www/src/lib/utils.ts
+++ apps/www/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export type CarouselCard = {ref: string, title: string, description: string}
+
+export type Project = {title: string; href: string; description: string};
